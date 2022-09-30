51c3efba81853396f79b0a09157db6a439d78828
Oleh Medvid <olmedvid@gmail.com>
2022-10-01 02:25:45 +0300

    add: navbar and styles to navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
 import { CTA, Brand, Navbar } from "./components";
-
+import './App.css';
 
 const App = () => {
   return (
